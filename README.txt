69238a66eb6ee87efbb7ef57586bed7b536c722b
Yhatoh <gacarmonat@gmail.com>
2019-12-22 23:37:53 -0300

    estado inicial

---
+++ README.txt
@@ -0,0 +1,3 @@
+Nombres:    Gabriel Carmona // Jorge Ludueña
+Roles:      201773509-0     // 201773507-4
+
---
+++ src/Tarea2/Problema1.java
@@ -0,0 +1,10 @@
+package tarea3;
+
+public class Problema1 {
+
+	public static void main(String[] args) {
+		System.out.println("asdasd");
+	}
+}
+
+
