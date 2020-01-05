8dc4ba4d5378ac9d5f640efcfd1fe3d51c80fedc
YorkoDev <jorge.luduena@sansano.usm.cl>
2020-01-05 17:07:51 -0300

    Comentario de la funcion printa (se me olvido guardarlo xd)

--- prob2/src/Tarea2/printa.java
+++ prob2/src/Tarea2/printa.java
@@ -1,7 +1,16 @@
 package tarea2;
 
+
+
 public class printa
 {
+/*
+	Funcion Printa(int [] arr)
+
+	Input: Arreglo de enteros
+	Descripcion: Muestra por pantalla un arreglo, entre [] y separando los datos por comas.
+	No tiene output
+*/
 	public void printas(int [] arr)
 	{
 		System.out.print("[");
